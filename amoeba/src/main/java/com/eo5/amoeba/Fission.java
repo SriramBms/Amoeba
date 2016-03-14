31810b0a7b5b729186c37a68d54d4f268b8fc2b3
SriramBms <sram.bms@gmail.com>
2016-03-14 11:01:09 -0400

    Initial working FAB

---
+++ amoeba/src/main/java/com/eo5/amoeba/Fission.java
@@ -0,0 +1,9 @@
+package com.eo5.amoeba;
+
+/**
+ * Created by Sriram on 3/11/2016.
+ *
+ */
+public class Fission {
+
+}
---
+++ amoeba/src/main/java/com/eo5/amoeba/FloatingFissionButton.java
@@ -0,0 +1,23 @@
+package com.eo5.amoeba;
+
+
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+
+/**
+ * Created by Sriram on 3/11/2016.
+ */
+public class FloatingFissionButton extends FloatingActionButton{
+    public FloatingFissionButton(Context context) {
+        this(context, null);
+    }
+    public FloatingFissionButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    public FloatingFissionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+    }
+
+}
