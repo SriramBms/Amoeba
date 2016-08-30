4c6e4124b7808eeeb1918ebc88675d9d6cb24a5a
SriramBms <sram.bms@gmail.com>
2016-08-29 23:22:31 -0400

    test rig

--- amoeba/src/test/java/com/eo5/amoeba/ExampleUnitTest.java
+++ amoeba/src/test/java/com/eo5/amoeba/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.eo5.amoeba;
 
+import android.util.Log;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,5 +13,6 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        Log.v("TEST","Test complete");
     }
 }
---
+++ app/src/test/java/com/eo5/amoebaexample/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.eo5.amoebaexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Sriram on 6/3/2016.
+ */
+public class MainActivityTest {
+    @Test
+    public void dummytest_1() throws Exception {
+        assertEquals(1,1);
+    }
+}
